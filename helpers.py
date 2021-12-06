2915ad50aadc186deb8dc2a8bf17f628e9cec773
jwillekesmct <94389770+jwillekesmct@users.noreply.github.com>
2021-12-06 20:55:48 +0100

    - Added a function to describe the dataset - Added a function to clean the data set by:   * set the model names as numbers   * get the titles out of the full dict   * standardize the titels such that key terms are written similarly (e.j. '"', 'inch', 'Inch' all to 'inch')   * Only leave the 'model words' in the titles, such that we can compare them more easily

--- helpers.py
+++ helpers.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
+import re
 
-
-def count_descriptions(data: dict)->int:
+def count_descriptions(data: dict) -> int:
     """Calculate number of descriptions of the dataset
 
     Args:
@@ -16,4 +16,30 @@ def count_descriptions(data: dict)->int:
         descriptions += len(data[key])
     return descriptions
 
-def number_desc
+
+def number_models(data: dict) -> dict:
+    model = {}
+    i = 0
+    for key in data.keys():
+        for description in data[key]:
+            model[i] = description
+            i += 1
+    return model
+
+
+def get_titles(models: dict) -> dict:
+    titles = {}
+    for key in models.keys():
+        titles[key] = models[key]['title']
+    return titles
+
+
+def standardize_titles(titles: dict) -> dict:
+    expr = '[a-zA-Z0-9.]*[0-9]+[a-zA-Z0-9.]*'
+    for key in titles.keys():
+        titles[key] = titles[key].replace('"','inch')
+        titles[key] = titles[key].lower()
+        titles[key] = re.sub("[^a-zA-Z0-9\s\.]","", titles[key])
+        titles[key] = re.findall(expr, titles[key])
+    return titles
+
